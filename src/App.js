b32ec5c8efa1277a558f406b09ee3bc1567aa2b9
Nadeem <anwarnadeem2010@gmail.com>
2022-08-19 12:18:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Deploy by Nadeem</p>
+        <p>Deployed through Github</p>
       </header>
     </div>
   );
